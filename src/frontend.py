637cfb84c9b8acd63450fca5c9dfb507a187b322
Pascal Berger <phyti1phyti1@gmail.com>
2022-11-24 22:19:47 +0100

    frontend bugfixes, added data. Ref #6

--- src/frontend.py
+++ src/frontend.py
@@ -16,6 +16,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 # Adds chromedriver binary to path
 import chromedriver_binary
+from console_progressbar import ProgressBar
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -39,7 +40,8 @@ def init_selenium(timeout):
 
 def read_data_from_url(driver, url, i_page):
   page_load_timeout_s = 2
-  df = pd.DataFrame()
+  df_page = pd.DataFrame()
+  i_article = -1
   try:
     driver.get(url)
     
@@ -53,19 +55,24 @@ def read_data_from_url(driver, url, i_page):
     if(len(news) == 0):
         raise Exception("no news articles found")
 
-    for i, news_article in enumerate(news):
-      df.loc[df, "i_page"] = i_page
-      df.loc[df, "href"] = news_article.find_element(By.CLASS_NAME, "_2PHmKV2J").get_attribute("href")
-      df.loc[df, "teaser"] = news_article.find_element(By.CLASS_NAME, "_2TpfbHst").get_attribute("innerText")
-      df.loc[df, "title"] = news_article.find_element(By.CLASS_NAME, "_1w8f1Lsk").get_attribute("innerText")
-      df.loc[df, "date"] = news_article.find_element(By.CLASS_NAME, "_2kyH0Dhp").get_attribute("innerText")
-      df.loc[df, "category"] = news_article.find_element(By.CLASS_NAME, "_25SLsqeV").get_attribute("innerText")
+    for i_article, news_article in enumerate(news):
+      df_page.loc[i_article, "i_page"] = i_page
+      df_page.loc[i_article, "href"] = news_article.find_element(By.CLASS_NAME, "_2PHmKV2J").get_attribute("href")
+      try:
+        df_page.loc[i_article, "teaser"] = news_article.find_element(By.CLASS_NAME, "_2TpfbHst").get_attribute("innerText")
+      except:
+        df_page.loc[i_article, "teaser"] = ""
+        print(f"<{url}>, article <{i_article}>. No Teaser present.")
+      
+      df_page.loc[i_article, "title"] = news_article.find_element(By.CLASS_NAME, "_1w8f1Lsk").get_attribute("innerText")
+      df_page.loc[i_article, "date"] = news_article.find_element(By.CLASS_NAME, "_2kyH0Dhp").get_attribute("innerText")
+      df_page.loc[i_article, "category"] = news_article.find_element(By.CLASS_NAME, "_25SLsqeV").get_attribute("innerText")
     
-    return df
+    return df_page
 
   except Exception as e:
     print(e)
-    print(f"Scraper failed at <{url}>. Error: {traceback.print_exc()}")
+    print(f"<{url}>, article <{i_article}>. Error: {traceback.print_exc()}")
     raise e
     
 
@@ -73,28 +80,32 @@ def scrape_cash_ch():
   buffer_timeout_s = 1
   max_fails = 3
   fails = 0
+  max_pages = 12350
 
   # initialize service
   driver = init_selenium(buffer_timeout_s)
 
-  # load previous data
+  # initial setup
   df = pd.DataFrame()
+  pb = ProgressBar(total=100, decimals=3, length=50)
 
   # scrape data
-  for i_page in range(1, 2000, 1):
+  for i_page in range(1, max_pages):
     url = f"https://www.cash.ch/news/alle?page={i_page}"
     try:
-      df_new = read_data_from_url(driver, url, i_page)
-      df = pd.concat([df, df_new], axis=1)
+      df_page = read_data_from_url(driver, url, i_page)
+      fails = 0
+      df = pd.concat([df, df_page], axis=0)
 
       # save, overwrite data
       backend.save_df_as_csv(df)
+      pb.print_progress_bar(i_page / max_pages * 100)
     except:
       fails += 1
       if(fails > max_fails):
         break
 
 
-
 if(__name__ == '__main__'):
   scrape_cash_ch()
+
